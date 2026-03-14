94ac7d596b9ebcba9b68bbedb72cbfcb4b63d95e
MohammedMqat <mohammedmqat@gmail.com>
2026-03-14 21:25:56 +0000

    fix server lint errors

--- src/router.js
+++ src/router.js
@@ -7,47 +7,55 @@ const wordsArray = wordsBuffer.toString().split("\n");
 
 const PUBLIC_DIR = path.join(__dirname, "..", "public");
 const MIME = {
+  ".css": "text/css",
   ".html": "text/html",
   ".js": "application/javascript",
-  ".css": "text/css",
 };
 
-function serveStatic(res, filePath) {
+const NOT_FOUND_HTTP_STATUS_CODE = 404;
+const OK_HTTP_STATUS_CODE = 200;
+
+const serveStatic = (res, filePath) => {
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(404);
+      res.writeHead(NOT_FOUND_HTTP_STATUS_CODE);
       res.end("Not found");
       return;
     }
     const ext = path.extname(filePath);
-    res.writeHead(200, { "Content-Type": MIME[ext] || "text/plain" });
+    res.writeHead(OK_HTTP_STATUS_CODE, { "Content-Type": MIME[ext] || "text/plain" });
     res.end(data);
   });
 }
 
-module.exports = function router(req, res) {
+const registerLogger = (req,res) => {
   const start = Date.now();
-  const { pathname, query } = url.parse(req.url, true);
-
   res.on("finish", () => {
     const ms = Date.now() - start;
     console.log(`${req.method} ${req.url} ${res.statusCode} ${ms}ms`);
   });
-  //("\n");
-  if (pathname === "/api/autocomplete") {
-    const q = query.q;
-    const results = wordsArray
-      .filter((words) => {
-        return words.startsWith(q);
-      })
-      .slice(0, 10);
-    // TODO: implement autocomplete logic using query.q
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ results }));
+}
 
-    return;
+module.exports = (req, res) => {
+  registerLogger(req,res);
+  const { pathname, query } = url.parse(req.url, true);
+
+  if (pathname === "/api/autocomplete") {
+    return handleAutoComplete(query, res);
   }
 
   const file = pathname === "/" ? "index.html" : pathname.slice(1);
   serveStatic(res, path.join(PUBLIC_DIR, file));
 };
+
+const handleAutoComplete = (query, res) => {
+  const searchValue = query.q;
+  const results = wordsArray
+    .filter((words) => words.startsWith(searchValue))
+    .slice(0, 10);
+  res.writeHead(OK_HTTP_STATUS_CODE, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ results }));
+
+  return;
+}
+
